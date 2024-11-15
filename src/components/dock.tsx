bb2efa3c4f62b113d2ac8617dc7d02bc37a7b701
akuldeepj <akuldeepj@gmail.com>
2024-11-16 02:20:38 +0530

    dock

---
+++ src/components/dock.tsx
@@ -0,0 +1,64 @@
+"use client"
+import React from "react";
+import { FloatingDock } from "@/components/ui/floating-dock";
+import {
+  // IconBrandGithub,
+  // IconBrandX,
+  IconExchange,
+  IconHome,
+  IconBrandInstagram,
+  IconBook,
+  IconTimeline,
+  Icon24Hours
+} from "@tabler/icons-react";
+// import Image from "next/image";
+
+export default function FloatingDockBar() {
+  const links = [
+    {
+      title: "Home",
+      icon: (
+        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#home",
+    },
+
+    {
+      title: "Rules",
+      icon: (
+        <IconBook className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#rules",
+    },
+    {
+      title: "Timeline",
+      icon: (
+        <IconTimeline className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#timeline",
+    },
+    
+    {
+      title: "Problem Statements",
+      icon: (
+        <Icon24Hours className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "/ps",
+    },
+
+    // {
+    //   title: "Instagram",
+    //   icon: (
+    //     <IconBrandInstagram className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    //   ),
+    //   href: "https://www.instagram.com/recurse.official",
+    // },
+  ];
+  return (
+    <div className="flex items-center justify-center w-full fixed z-50 bottom-5">
+      <FloatingDock
+        items={links}
+      />
+    </div>
+  );
+}
